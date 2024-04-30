e76ac4bab1dd3d9dba26c9f41cea954318324055
DeanT <dean@germs.dev>
2024-04-30 19:14:31 +0100

    more packets

--- index.md
+++ index.md
@@ -24,4 +24,47 @@ Command line monitoring and summarising of network traffic.
 - [x] Use CMake
 - [x] Use latest clang
 
+## Review
+
+```cpp
+  // // Set a filter (optional)
+  // struct bpf_program filter;
+  // pcap_compile(pcapHandle, &filter, "tcp port 80", 0, PCAP_NETMASK_UNKNOWN);
+  // pcap_setfilter(pcapHandsle, &filter);
+
+  // // Start capturing packets
+  // std::cout << "Capturing packets..." << std::endl;
+  // pcap_loop(pcapHandle, 0, packetHandler, nullptr);
+
+  // // Close the pcap handle when done
+  // pcap_close(pcapHandle);
+
+// static_assert(not std::empty(get_quote());
+
+
+// // file header
+// struct pcap_file_header {
+//   std::uint32_t magic_number;  // Magic number
+//   std::uint16_t version_major; // Major version number
+//   std::uint16_t version_minor; // Minor version number
+//   std::int32_t thiszone;       // GMT to local correction
+//   std::uint32_t sigfigs;       // Accuracy of timestamps
+//   std::uint32_t snaplen;       // Max length of captured packets
+//   std::uint32_t network;       // Data link type
+// };
+
+// // packet header
+// struct pcap_packet_header {
+//   std::uint32_t ts_sec;   // Timestamp seconds
+//   std::uint32_t ts_usec;  // Timestamp microseconds
+//   std::uint32_t incl_len; // Number of octets of packet saved in file
+//   std::uint32_t orig_len; // Actual length of packet
+// };
+
+// std::atexit([]() {
+//   std::println("cya!");
+//   return 0;
+// });
+```
+
 ---
--- src/main.cxx
+++ src/main.cxx
@@ -2,9 +2,9 @@
 #include <cassert>
 #include <pcap.h>
 #include <print>
+#include <ranges>
 #include <string>
 #include <string_view>
-#include <ranges>
 
 constexpr std::array quotes{
     "Hello? Is it me you're looking for?",
@@ -17,7 +17,6 @@ constexpr std::array quotes{
 };
 
 constexpr auto get_quote() {
-
   // Get random index
   auto index = std::rand() % quotes.size();
   assert(index < quotes.size());
@@ -26,38 +25,12 @@ constexpr auto get_quote() {
   return quotes[std::rand() % quotes.size()];
 }
 
-static_assert(not std::empty(quotes));
-
-// // file header
-// struct pcap_file_header {
-//   std::uint32_t magic_number;  // Magic number
-//   std::uint16_t version_major; // Major version number
-//   std::uint16_t version_minor; // Minor version number
-//   std::int32_t thiszone;       // GMT to local correction
-//   std::uint32_t sigfigs;       // Accuracy of timestamps
-//   std::uint32_t snaplen;       // Max length of captured packets
-//   std::uint32_t network;       // Data link type
-// };
-
-// // packet header
-// struct pcap_packet_header {
-//   std::uint32_t ts_sec;   // Timestamp seconds
-//   std::uint32_t ts_usec;  // Timestamp microseconds
-//   std::uint32_t incl_len; // Number of octets of packet saved in file
-//   std::uint32_t orig_len; // Actual length of packet
-// };
-
-// std::atexit([]() {
-//   std::println("cya!");
-//   return 0;
-// });
-
 void capture(std::string_view network_device) {
 
   char errbuf[256];
 
   //   constexpr auto network_device = +"en0";
-  std::println("Opening network device: '{}'", network_device);
+  std::println(stderr, "Opening network device: '{}'", network_device);
 
   // Open the network device in promiscuous mode
   pcap_t *pcap = pcap_open_live(std::string{network_device}.c_str(), 65535, 1,
@@ -69,51 +42,43 @@ void capture(std::string_view network_device) {
     return;
   }
 
-  for (auto _ : std::views::iota(0, 9)) {
+  for (auto _ : std::views::iota(0, 200)) {
 
     // Read packets
     pcap_pkthdr header;
     const u_char *data = pcap_next(pcap, &header);
 
     if (data == nullptr) {
-      std::println("{}", get_quote());
+      std::println(stderr, "{}", get_quote());
       return;
     }
 
     // Print device name
-    std::print("{}\t", network_device);
+    std::print(stderr, "{}\t", network_device);
 
     // print packet data
     for (auto i = size_t{}; i < header.len; ++i) {
 
       // Print the mac address
       if (i < 6) {
-        std::print("{:02x}", data[i]);
-        if (i < 5) {
+        std::print(stderr, "{:02x}", data[i]);
+
+        if (i < 5)
           std::print(":");
-        }
-      } else {
+      } else
         break;
-      }
     }
 
+    // print packet type
+    std::print(stderr, "\t- {:02x}{:02x}", data[12], data[13]);
+
     // print ip addresses
-    std::print(" - {}.{}.{}.{}", data[26], data[27], data[28], data[29]);
+    std::print(stderr, " - {}.{}.{}.{}", data[26], data[27], data[28],
+               data[29]);
 
     // print packet length
-    std::println("\t- {} bytes", header.len);
+    std::println(stderr, "\t- {} bytes", header.len);
   }
-  // // Set a filter (optional)
-  // struct bpf_program filter;
-  // pcap_compile(pcapHandle, &filter, "tcp port 80", 0, PCAP_NETMASK_UNKNOWN);
-  // pcap_setfilter(pcapHandsle, &filter);
-
-  // // Start capturing packets
-  // std::cout << "Capturing packets..." << std::endl;
-  // pcap_loop(pcapHandle, 0, packetHandler, nullptr);
-
-  // // Close the pcap handle when done
-  // pcap_close(pcapHandle);
 }
 
 int main() {
@@ -133,15 +98,12 @@ int main() {
   for (pcap_if_t *d = alldevs; d != nullptr; d = d->next)
     std::println("\t{}", d->name);
 
-//   while (true)
-    for (pcap_if_t *d = alldevs; d != nullptr; d = d->next) {
+  for (pcap_if_t *d = alldevs; d != nullptr; d = d->next) {
 
-      std::println("\t{}", d->name);
-      capture(d->name);
+    std::println("\t{}", d->name);
+    capture(d->name);
+    break;
+  }
 
-        break;
-      // std::jthread(std::launch::async, capture, d->name);
-    }
-
-    std::println("cya!");
+  std::println("cya!");
 }
