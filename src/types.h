84ac7909d2b01762b2c0deb99d8468aaaf00f09e
DeanT <dean@germs.dev>
2024-05-23 21:43:14 +0100

    init [skip ci]

--- src/types.h
+++ src/types.h
@@ -25,16 +25,16 @@ struct ethernet_packet_t {
 
 // IPv4 structure
 struct ip_header_t {
-  uint8_t version_ihl;
-  uint8_t dscp_ecn;
-  uint16_t total_length;
-  uint16_t identification;
-  uint16_t flags_fragment_offset;
-  uint8_t ttl;
-  uint8_t protocol;
-  uint16_t checksum;
-  uint8_t source_ip[4];
-  uint8_t dest_ip[4];
+  uint8_t version_ihl{};
+  uint8_t dscp_ecn{};
+  uint16_t total_length{};
+  uint16_t identification{};
+  uint16_t flags_fragment_offset{};
+  uint8_t ttl{};
+  uint8_t protocol{};
+  uint16_t checksum{};
+  uint8_t source_ip[4]{};
+  uint8_t dest_ip[4]{};
 };
 
 static_assert(sizeof(ip_header_t) == 20);
