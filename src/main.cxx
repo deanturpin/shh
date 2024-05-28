90dac4e0ec01528574d41f79e91255d106d8c5e4
DeanT <dean@germs.dev>
2024-05-28 15:47:54 +0100

    chore: Add logging statement to stop all threads

--- src/main.cxx
+++ src/main.cxx
@@ -104,6 +104,8 @@ int main() {
   // Capture packets for a while
   std::this_thread::sleep_for(60s);
 
+  std::println("Stopping {} threads", threads.size());
+
   // Stop all the threads
   for (auto &thread : threads) {
 
